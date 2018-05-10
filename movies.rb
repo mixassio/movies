a4f764e3b61c22a51766e4468dbb832fb2602e3c
mixassio <mixail.bondarev@gmail.com>
2018-05-10 15:11:50 +0300

    part 4

--- movies.rb
+++ movies.rb
@@ -1,3 +1,6 @@
+require 'CSV'
+require 'ostruct'
+
 file_name = ARGV[0] || './movies.txt'
 if !File.file?(file_name)
     abort "No such file"
@@ -5,18 +8,22 @@ end
 
 hash_keys = %i[link title year country date zhanre time rating director actors]
 
-hash_movies = IO.read(file_name).split("\n").map { |movie|
-    hash_keys.zip(movie.split("|")).to_h
+movies = CSV.readlines(file_name, :col_sep => '\n').flatten.map{|el| 
+    OpenStruct.new(hash_keys.zip(el.split("|")).to_h)
 }
+
+long_movies = movies.sort_by{|hsh| hsh[:time].split(' ')[0].to_i}.last(5)
+comedy_movies = movies.select{|el| el[:zhanre].include? "Comedy"}.sort_by{|hsh| hsh[:date]}.first(10)
+count_movies_not_USA = movies.reject{|el| el[:country].include? "USA"}.length
+directors = movies.map{|el| el[:director]}.uniq.sort_by{|el| el.split(' ').last(1)}
+
+months = movies.map{|el| el.date}.select{|el| el.length == 10}.map{|el| Date.parse(el).mon}
+num_months = [1,2,3,4,5,6,7,8,9,10,11,12]
+count_by_month = num_months.map{|el| {Date::MONTHNAMES[el] => months.count(el)}}
 
 def prettyMovies(listMovies)
     listMovies.map{|el| "#{el[:title]} (#{el[:date]}; #{el[:zhanre]}) - #{el[:time]}"}
 end
-
-long_movies = hash_movies.sort_by{|hsh| hsh[:time].split(' ')[0].to_i}.last(5)
-comedy_movies = hash_movies.select{|el| el[:zhanre].include? "Comedy"}.sort_by{|hsh| hsh[:date]}.first(10)
-count_movies_not_USA = hash_movies.reject{|el| el[:country].include? "USA"}.length
-directors = hash_movies.map{|el| el[:director]}.uniq.sort_by{|el| el.split(' ').last(1)}
 
 puts '************************************'
 puts '5 the longest movies:'
@@ -30,4 +37,7 @@ puts directors
 puts '************************************'
 puts 'Count movies, made not in USA:'
 puts count_movies_not_USA
+puts '************************************'
+puts 'Count movies, maded by month:'
+puts count_by_month
 puts '************************************'
