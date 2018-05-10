ed174c285b645355a1f9eb27b0d079db760290cf
mixassio <mixail.bondarev@gmail.com>
2018-05-10 17:22:37 +0300

    change part4

--- movies.rb
+++ movies.rb
@@ -8,21 +8,20 @@ end
 
 hash_keys = %i[link title year country date zhanre time rating director actors]
 
-movies = CSV.readlines(file_name, :col_sep => '\n').flatten.map{|el| 
-    OpenStruct.new(hash_keys.zip(el.split("|")).to_h)
+movies = CSV.readlines(file_name, :col_sep => '|', :headers => hash_keys).map{|movie|
+    OpenStruct.new(movie.to_h)
 }
 
-long_movies = movies.sort_by{|hsh| hsh[:time].split(' ')[0].to_i}.last(5)
-comedy_movies = movies.select{|el| el[:zhanre].include? "Comedy"}.sort_by{|hsh| hsh[:date]}.first(10)
-count_movies_not_USA = movies.reject{|el| el[:country].include? "USA"}.length
-directors = movies.map{|el| el[:director]}.uniq.sort_by{|el| el.split(' ').last(1)}
+long_movies = movies.sort_by{|hsh| hsh.time.split(' ')[0].to_i}.last(5)
+comedy_movies = movies.select{|el| el.zhanre.include? "Comedy"}.sort_by{|hsh| hsh.date}.first(10)
+count_movies_not_USA = movies.reject{|el| el.country.include? "USA"}.length
+directors = movies.map(&:director).uniq.sort_by{|el| el.split(' ').last(1)}
 
-months = movies.map{|el| el.date}.select{|el| el.length == 10}.map{|el| Date.parse(el).mon}
-num_months = [1,2,3,4,5,6,7,8,9,10,11,12]
-count_by_month = num_months.map{|el| {Date::MONTHNAMES[el] => months.count(el)}}
+months = movies.map(&:date).select{|el| el.length == 10}.map{|el| Date.parse(el).mon}
+count_by_month = months.group_by(&:itself).sort_by{|k, v| k}.map{|k, v| {Date::MONTHNAMES[k] => v.length}}
 
 def prettyMovies(listMovies)
-    listMovies.map{|el| "#{el[:title]} (#{el[:date]}; #{el[:zhanre]}) - #{el[:time]}"}
+    listMovies.map{|el| "#{el.title} (#{el.date}; #{el.zhanre}) - #{el.time}"}
 end
 
 puts '************************************'
